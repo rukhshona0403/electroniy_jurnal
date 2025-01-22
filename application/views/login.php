1e8af9e8303e5dcdfa8026edd2a3d62aa4377061
rukhshona0403 <rayimberdievarukhshona@gmail.com>
2025-01-22 22:59:01 +0500

    обновить

--- application/views/login.php
+++ application/views/login.php
@@ -46,18 +46,13 @@
                                         <div class="form-group">
                                             <input type="login" name='login' class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                                placeholder="Логин...">
                                         </div>
                                         <div class="form-group">
                                             <input type="password" name='password' class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                                id="exampleInputPassword" placeholder="Парол...">
                                         </div>
                                         <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
                                         </div>
                                         <button type="submit"  name="btnEnter" value="btnEnter" class="btn btn-primary btn-user btn-block" >
                                             Дохилшавӣ
@@ -66,9 +61,7 @@
                                         <a href="index.html" class="btn btn-google btn-user btn-block">
                                             <i class="fab fa-google fa-fw"></i> Login with Google
                                         </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
+                                        
                                     </form>
                                     <hr>
                                     <div class="text-center">
